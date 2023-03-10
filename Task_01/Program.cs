197852f8bc081c4a76d584261955594ee1622844
Tatsiana Putryk <tushkanchik85@gmail.com>
2023-03-10 20:45:18 +0300

    task_01 added

---
+++ Task_01/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 1: Задайте двумерный массив. Напишите программу, 
+// которая упорядочивает по убыванию элементы каждой строки двумерного массива.
+
+int InputUser(string message)
+{
+    Console.Write($"{message} => ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GenerateArray(int row, int col)
+{
+    int[,] array = new int[row, col]; // Создаем 2-мерный массив
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(-9, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.WriteLine();
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} \t");
+        }
+    }
+    Console.WriteLine();
+}
+
+int[,] NewArray(int[,] numbers)
+{
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            for (int k = 0; k < numbers.GetLength(1) - 1; k++)
+            {
+                if (numbers[i, k] > numbers[i, k + 1])
+                {
+                    int temp = 0;
+                    temp = numbers[i, k + 1];
+                    numbers[i, k + 1] = numbers[i, k];
+                    numbers[i, k] = temp;
+                }
+            }
+        }
+    }
+    return numbers;
+}
+
+int RowInput = InputUser("Введите количество строк двумерного массива: ");
+int ColInput = InputUser("Введите количество колонок двумерного массива: ");
+int[,] arrayGenerate = GenerateArray(RowInput, ColInput);
+Console.WriteLine("Сгенерированный массив: ");
+PrintArray(arrayGenerate);
+Console.WriteLine("Упорядоченный массив: ");
+PrintArray(NewArray(arrayGenerate));
